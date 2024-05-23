6079c2c6f803494d7df2823690799b239e637dc0
Maria Ivanova <moi066@mail.ru>
2024-05-23 21:05:55 +0300

    add filter_by_state in processing and README

---
+++ src/processing.py
@@ -0,0 +1,47 @@
+def filter_by_state(card_details: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+       Функция, которая принимает на вход список словарей и значение для ключа state
+    (опциональный параметр со значением по умолчанию EXECUTED) и возвращает новый список,
+    содержащий только те словари, у которых ключ state содержит переданное в функцию значение
+    """
+    filtered_card_details = []
+
+    for detail in card_details:
+        if detail["state"] == state:
+            filtered_card_details.append(detail)
+        else:
+            pass
+
+    return filtered_card_details
+
+
+if __name__ == "__main__":
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "CANCELED",
+        )
+    )
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
+
+    # Выход функции со статусом по умолчанию EXECUTED
+    # [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    #  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+    # Выход функции, если вторым аргументов передано 'CANCELED'
+    # [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    #  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
